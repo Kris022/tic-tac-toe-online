770bab4665bb951a0e3e9a97e36a395bd22905c0
Kris <kharonek@yahoo.com>
2023-08-19 14:05:55 -0300

    feat: add function to determine the winner

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -6,11 +6,21 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState<boolean>(true);
 
+  const winner = calculateWinner(squares);
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (isXNext ? "X" : "O");
+  }
+
   const handleClick = (i: number) => {
-    if (squares[i]) {
+    // check if user pressed on square that is occupied
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
-    
+
     const nextSquares = squares.slice(); // creates copy of squares array
     if (isXNext) {
       nextSquares[i] = "X"; //
@@ -22,8 +32,36 @@ export default function Board() {
     setSquares(nextSquares);
   };
 
+
+  function calculateWinner(squares: number[][]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   return (
     <>
+      <h1>{status}</h1>
       <div className="board-row">
         <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
         <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
