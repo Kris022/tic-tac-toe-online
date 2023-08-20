9a5c66a467de28c9ba219a1a106a110cea563a87
Kris <kharonek@yahoo.com>
2023-08-20 12:02:15 -0300

    feat: tie and reset

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -3,14 +3,21 @@ import { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const initialSquares = Array(9).fill(null);
+  const [squares, setSquares] = useState(initialSquares);
   const [isXNext, setIsXNext] = useState<boolean>(true);
 
   const winner = calculateWinner(squares);
 
   let status;
+  let gameOver: boolean = false;
+
   if (winner) {
     status = "Winner: " + winner;
+    gameOver = true;
+  } else if (isTie()) {
+    status = "Tie!";
+    gameOver = true;
   } else {
     status = "Next player: " + (isXNext ? "X" : "O");
   }
@@ -65,24 +72,42 @@ export default function Board() {
     return null;
   }
 
+  function isTie() {
+    for (let i = 0; i < squares.length; i++) {
+      if (!squares[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  const handleReset = () => {
+    setSquares(initialSquares);
+  };
+
   return (
     <div>
       <h1>{status}</h1>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
-        <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
-        <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+
+      <div>
+        <div className="board-row">
+          <Square onSquareClick={() => handleClick(0)} value={squares[0]} />
+          <Square onSquareClick={() => handleClick(1)} value={squares[1]} />
+          <Square onSquareClick={() => handleClick(2)} value={squares[2]} />
+        </div>
+        <div className="board-row">
+          <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
+          <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
+          <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
+        </div>
+        <div className="board-row">
+          <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
+          <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
+          <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(3)} value={squares[3]} />
-        <Square onSquareClick={() => handleClick(4)} value={squares[4]} />
-        <Square onSquareClick={() => handleClick(5)} value={squares[5]} />
-      </div>
-      <div className="board-row">
-        <Square onSquareClick={() => handleClick(6)} value={squares[6]} />
-        <Square onSquareClick={() => handleClick(7)} value={squares[7]} />
-        <Square onSquareClick={() => handleClick(8)} value={squares[8]} />
-      </div>
+
+      {gameOver && <button onClick={handleReset}>Reset</button>}
     </div>
   );
 }
